01726e2f551b20a5ec62828ad0885be8d869f842
tama <tama@ttk1.net>
2019-10-27 00:25:50 +0900

    BackupCommandExecutorTest作成

--- src/test/java/net/ttk1/serverbackup/BackupCommandExecutorTest.java
+++ src/test/java/net/ttk1/serverbackup/BackupCommandExecutorTest.java
@@ -3,14 +3,27 @@ package net.ttk1.serverbackup;
 import org.bukkit.command.CommandSender;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class BackupCommandExecutorTest {
     @Test
     public void onCommandTest() {
-        assertThat(true, is(true));
+        BackupTaskRunner backupTaskRunner = mock(BackupTaskRunner.class);
+        CommandSender sender = mock(CommandSender.class);
+        when(sender.hasPermission("serverbackup.backup")).thenReturn(true);
+        BackupCommandExecutor executor = new BackupCommandExecutor(backupTaskRunner);
+        executor.onCommand(sender, null, null, null);
+        verify(backupTaskRunner).runBackupTask(sender);
+    }
+
+    @Test
+    public void onCommandWithoutPermissionTest() {
+        BackupTaskRunner backupTaskRunner = mock(BackupTaskRunner.class);
+        CommandSender sender = mock(CommandSender.class);
+        when(sender.hasPermission("serverbackup.backup")).thenReturn(false);
+        BackupCommandExecutor executor = new BackupCommandExecutor(backupTaskRunner);
+        executor.onCommand(sender, null, null, null);
+        verify(backupTaskRunner, never()).runBackupTask(any());
+        verify(sender).sendMessage("コマンドを実行するための権限がありません。");
     }
 }
